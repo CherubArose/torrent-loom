952881644531de3d9640ad7e7b2855cbe5fe346e
Cherub Arose <163398193+CherubArose@users.noreply.github.com>
2024-03-16 16:24:04 +1100

    Load module from TorrentLoom

--- src/main/kotlin/org/torrentloom/Configuration.kt
+++ src/main/kotlin/org/torrentloom/Configuration.kt
@@ -3,38 +3,37 @@ package org.torrentloom
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.polymorphic
-import org.koin.core.context.startKoin
 import org.koin.core.module.Module
-import org.koin.core.qualifier.named
+import org.koin.dsl.koinApplication
 import org.koin.dsl.module
 import org.torrentloom.mediadata.MediaData
 import org.torrentloom.mediadata.MediaDataModule
 import org.torrentloom.mediadata.guessit.GuessItModule
+import org.torrentloom.mediadata.tmdb.TmdbModule
 
 object Configuration {
-    private val mediaDataModules = listOf(GuessItModule)
+    private val mediaDataModules = listOf(
+        GuessItModule,
+        TmdbModule,
+    )
 
     private val module: Module = module {
-        single<List<MediaDataModule<*>>>(named("modules")) { mediaDataModules }
+        single<List<MediaDataModule<out MediaData>>> { mediaDataModules }
         single<Json> {
             Json {
                 prettyPrint = true
                 classDiscriminator = "#class"
                 serializersModule = SerializersModule {
                     polymorphic(MediaData::class) {
-                        get<List<MediaDataModule<*>>>(named("modules")).onEach { it.registerMediaDataFormat(this) }
+                        get<List<MediaDataModule<MediaData>>>().map { it.getMediaDataFormat() }
+                            .onEach { (clazz, serializer) -> subclass(clazz, serializer) }
                     }
                 }
             }
         }
+
+        includes(mediaDataModules.map { it.defaultModuleConfiguration })
     }
 
-    fun load() {
-        startKoin {
-            modules(
-                module,
-                *mediaDataModules.map { it.defaultModuleConfiguration }.toTypedArray(),
-            )
-        }
-    }
+    val koin = koinApplication { modules(module) }.koin
 }
--- src/main/kotlin/org/torrentloom/command/TorrentLoom.kt
+++ src/main/kotlin/org/torrentloom/command/TorrentLoom.kt
@@ -4,9 +4,9 @@ import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import org.koin.core.Koin
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
-import org.koin.core.qualifier.named
 import org.torrentloom.Configuration
 import org.torrentloom.loom.Shuttle
 import org.torrentloom.mediadata.MediaDataModule
@@ -17,16 +17,14 @@ object TorrentLoom : CliktCommand(), KoinComponent {
     /**
      * List of all the mediaDataModule that will be run in order.
      */
-    private val mediaDataModules: List<MediaDataModule<*>> by inject(named("modules"))
+    private val mediaDataModules: List<MediaDataModule<*>> by inject()
     private val json: Json by inject()
-
-    init {
-        Configuration.load()
-    }
 
     override fun run() {
         mediaDataModules
             .fold(Shuttle(path = pathArgument)) { shuttle, module -> module.runModule(shuttle) }
             .let { println(json.encodeToString(it)) }
     }
+
+    override fun getKoin(): Koin = Configuration.koin
 }
